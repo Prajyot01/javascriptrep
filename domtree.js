04bfc8fe38b8f6a6446822f8ecbd0c0f693335f9
Prajyot01 <104674228+Prajyot01@users.noreply.github.com>
2022-05-01 14:02:01 +0530

    Add files via upload

---
+++ domtree.js
@@ -0,0 +1,31 @@
+// const rootelement= document.getRootNode()
+// // console.log(rootelement);
+
+// const htmlelement= rootelement.childNodes[0];
+// // console.log(htmlelement);
+
+// const childs=htmlelement.childNodes;
+// // console.log(childs)
+
+// // const headelementnode=htmlelement.childNodes[0];
+// // console.log(headelementnode)
+
+// const textelementnode=htmlelement.childNodes[1]; // textelement means space + newline
+// console.log(textelementnode)
+
+// const bodyelementnode=htmlelement.childNodes[2];
+// console.log(bodyelementnode)
+
+// console.log(headelementnode.parentElement)
+// console.log(headelementnode.parentNode)
+// console.log(headelementnode.nextSibling.nextSibling);
+// console.log(headelementnode.nextElementSibling)// it ignore all spaces and new line it gives next element
+
+// console.log(headelementnode.childNodes)
+// console.log(headelementnode.children)//it ignore all spaces and new line it gives next element
+
+
+
+
+
+
